c3efd6439b1150d154efcbe37945c1a5a6f2278f
Mariusz Wiazowski <mariusz.wiazowski@gmail.com>
2016-10-29 21:32:15 +0200

    Base di dati consolidata;

--- api/models/Product.js
+++ api/models/Product.js
@@ -11,7 +11,32 @@ module.exports = {
 
     nome: {type: 'string', required: true},
 
-    latestPrice: {type: 'number'},
+    valuta: {type: 'string', required: true},
+
+    '2014-11': {type: 'number'},
+    '2014-12': {type: 'number'},
+    '2015-01': {type: 'number'},
+    '2015-02': {type: 'number'},
+    '2015-03': {type: 'number'},
+    '2015-04': {type: 'number'},
+    '2015-05': {type: 'number'},
+    '2015-06': {type: 'number'},
+    '2015-07': {type: 'number'},
+
+    '2015-08': {type: 'number'},
+    '2015-09': {type: 'number'},
+    '2015-10': {type: 'number'},
+    '2015-11': {type: 'number'},
+    '2015-12': {type: 'number'},
+    '2016-01': {type: 'number'},
+    '2016-02': {type: 'number'},
+    '2016-03': {type: 'number'},
+    '2016-04': {type: 'number'},
+    '2016-05': {type: 'number'},
+    '2016-06': {type: 'number'},
+    '2016-07': {type: 'number'},
+    '2016-08': {type: 'number'},
+    '2016-09': {type: 'number'},
 
     // product list
     listProducts : {
